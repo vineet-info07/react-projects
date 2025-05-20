fd11ca626b819d3875919dfda6a8b1c540ecee90
Vineet Tijare <vineettijare@Vineets-MacBook-Air.local>
2025-05-20 17:28:37 +0530

    bank app

--- react-quiz/src/main.jsx
+++ react-quiz/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import BankAccount from "./components/BankAccount/BankAccount.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    {/* <BankAccount /> */}
   </StrictMode>
 );
