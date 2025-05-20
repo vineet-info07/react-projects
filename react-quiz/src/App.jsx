188badded9e267e4021dae75396b2972e290249e
Vineet Tijare <vineettijare@Vineets-MacBook-Air.local>
2025-05-20 14:58:51 +0530

    adding Timer with help of useEffect hook

--- react-quiz/src/App.jsx
+++ react-quiz/src/App.jsx
@@ -9,6 +9,10 @@ import Question from "./components/Question";
 import NextButton from "./components/NextButton";
 import "./index.css";
 import FinishScreen from "./components/FinishScreen";
+import Timer from "./components/Timer";
+import Footer from "./components/Footer";
+
+const SECS_PER_QUESTION = 30;
 
 const initialState = {
   questions: [],
@@ -18,6 +22,7 @@ const initialState = {
   answer: null,
   points: 0,
   highscore: 0,
+  secondsRemaining: null,
 };
 
 const reducer = (state, action) => {
@@ -37,6 +42,7 @@ const reducer = (state, action) => {
       return {
         ...state,
         status: "active",
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
       };
     case "newAnswer": {
       const question = state.questions.at(state.index);
@@ -66,14 +72,22 @@ const reducer = (state, action) => {
         questions: state.questions,
         status: "ready",
       };
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
+      };
     default:
       throw new Error("Action unknown");
   }
 };
 
 function App() {
-  const [{ questions, status, index, answer, points, highscore }, dispatch] =
-    useReducer(reducer, initialState);
+  const [
+    { questions, status, index, answer, points, highscore, secondsRemaining },
+    dispatch,
+  ] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
   const maxPossiblePoints = questions.reduce(
@@ -115,12 +129,15 @@ function App() {
               answer={answer}
               points={points}
             />
-            <NextButton
-              dispatch={dispatch}
-              answer={answer}
-              index={index}
-              numQuestions={numQuestions}
-            />
+            <Footer>
+              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
+              <NextButton
+                dispatch={dispatch}
+                answer={answer}
+                index={index}
+                numQuestions={numQuestions}
+              />
+            </Footer>
           </>
         )}
         {status === "finished" && (
--- react-quiz/src/components/Question.jsx
+++ react-quiz/src/components/Question.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import Options from "./Options";
 
-export default function Question({ question, dispatch, answer, points }) {
-  console.log(question);
-  console.log(points);
-
+export default function Question({ question, dispatch, answer }) {
   return (
     <div>
       <h4>{question.question}</h4>
