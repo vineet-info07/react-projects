4b6eaf7f73d8acaf0b047a982a371f5a1a4e385d
Vineet Tijare <vineettijare@Vineets-MacBook-Air.local>
2025-05-21 16:01:32 +0530

    Building application pages

---
+++ worldwise/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import PageNav from "../components/PageNav";
+
+function Login() {
+  return (
+    <div>
+      <PageNav />
+      <h1> Welcome to login page </h1>
+      <p> Under constuction...</p>
+    </div>
+  );
+}
+
+export default Login;
