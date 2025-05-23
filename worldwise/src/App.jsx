49b178452d775cfebc2ba7e5a4b67fda181af04e
Vineet Tijare <vineettijare@Vineets-MacBook-Air.local>
2025-05-23 15:57:17 +0530

    Programmatic Navigation with <Navigate />

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,15 +1,15 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
-import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
-import City from "./components/City";
 import PageNotFound from "./pages/PageNotFound";
+import CityList from "./components/CityList";
 import Form from "./components/Form";
+import City from "./components/City";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -41,10 +41,7 @@ export default function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
