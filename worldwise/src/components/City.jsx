8dcfaf3126d5054fb667fe749e02558a498d2130
Vineet Tijare <vineettijare@Vineets-MacBook-Air.local>
2025-05-23 14:36:43 +0530

    Reading and Setting a Query String

--- worldwise/src/components/City.jsx
+++ worldwise/src/components/City.jsx
@@ -1,11 +1,19 @@
-import { useParams } from "react-router-dom";
+/* eslint-disable no-unused-vars */
+import { useParams, useSearchParams } from "react-router-dom";
 
 function City() {
   const { id } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
 
   return (
     <div>
       <h1>City {id}</h1>
+      <p>
+        Position: {lat}, {lng}
+      </p>
     </div>
   );
 }
--- worldwise/src/components/CityItem.jsx
+++ worldwise/src/components/CityItem.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
 
@@ -11,11 +12,16 @@ const formatDate = (date) =>
 // eslint-disable-next-line react/prop-types
 export default function CityItem({ city }) {
   // eslint-disable-next-line react/prop-types
-  const { cityName, emoji, date, id } = city;
+  const { cityName, emoji, date, id, position } = city;
+
+  console.log(position);
 
   return (
     <li>
-      <Link className={styles.cityItem} to={`${id}`}>
+      <Link
+        className={styles.cityItem}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
