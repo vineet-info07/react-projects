55aa6a3abd57f2a550ec1f594ee5ba01d4ef5d73
Vineet Tijare <vineettijare@Vineets-MacBook-Air.local>
2025-05-26 07:08:12 +0530

    Creating a cities context

---
+++ worldwise/src/context/CitiesContext.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable react/prop-types */
+import { createContext, useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:8000";
+
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (err) {
+        console.error(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider value={{ cities, isLoading }}>
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+export { CitiesProvider };
