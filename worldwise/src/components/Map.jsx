2d7180f0f1133b406d6f1e7c68aacb7515789ba1
Vineet Tijare <vineettijare@Vineets-MacBook-Air.local>
2025-06-15 06:49:13 +0530

    setting a map position with  geolocation

--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -12,14 +12,19 @@ import {
 
 import styles from "./Map.module.css";
 import { useCities } from "../context/CitiesContext";
+import { useGeolocation } from "../hooks/useGeolocation";
+import Button from "./Button";
 
 export default function Map() {
   const navigate = useNavigate();
   const { cities } = useCities();
-
+  const {
+    isLoading: isLoadingPosition,
+    position: geolocationPosition,
+    getPosition,
+  } = useGeolocation();
   const [mapPosition, setMapPosition] = useState([40, 0]);
   const [searchParams] = useSearchParams();
-
   const mapLat = searchParams.get("lat");
   const mapLng = searchParams.get("lng");
 
@@ -30,8 +35,21 @@ export default function Map() {
     [mapLat, mapLng]
   );
 
+  useEffect(
+    function () {
+      if (geolocationPosition)
+        setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+    },
+    [geolocationPosition]
+  );
+
   return (
     <div className={styles.mapContainer}>
+      {!geolocationPosition && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingPosition ? "Loading..." : "Use your position"}
+        </Button>
+      )}
       <MapContainer
         center={mapPosition}
         zoom={6}
